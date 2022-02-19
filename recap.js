c0a5579abc44a6a34038e1925148c326f9d7a6a7
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-19 22:07:13 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,24 @@
+//let and const
+//const can't be changed.
+const hubby = "omor sani";
+//let cant be changed.
+let phone = "iphone 15";
+phone = "samsung galaxy s25"
+
+//default parameter //spread operator
+function maxNumber(array=[]){
+    const max = Math.max(...array);
+    return max;
+}
+
+const biggest = maxNumber();
+console.log(biggest);
+
+//template string
+//back tick instead of quotation
+const myNotes = `I am mojnu. ${hubby} I dont have a laili.`
+console.log(myNotes);
+
+//arrow function
+const square = x => x * x;
+console.log(square(8));
