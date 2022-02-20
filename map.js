ee1f8532d6e18d52dadd70fb8b425dcf0ed565ef
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-20 10:11:54 +0600

    forEach

---
+++ map.js
@@ -0,0 +1,39 @@
+const numbers = [4,5,9,10];
+const output2 = [];
+
+// old way
+// function doubleOld(number){
+//     return number * 2;
+// } 
+
+const doubleIt = number => number * 2;
+
+for(const number of numbers){
+    const result = doubleIt(number)
+    output2.push(result);
+}
+
+console.log(output2);
+
+
+//map
+//above work in shortcut;
+//===========================================================
+//1. loop through each element
+//2. for each element call the provided function.
+//3. result for each element will be stored in an array.
+
+const output = numbers.map(doubleIt);
+console.log(output);
+//or
+
+const output3 = numbers.map(number => number * 2)
+console.log(output3);
+
+//or
+const output4 = numbers.map(x => x * 2)
+console.log(output4);
+
+//or
+const squares = numbers.map(x=> x * x);
+console.log(squares);
---
+++ more-map.js
@@ -0,0 +1,32 @@
+//find out the length of the array
+const friend = ["Tom Hanks", "Tom Cri", "Tom Brady"];
+
+const fLengths = friend.map(friend => friend.length);
+console.log(fLengths);
+
+//mor example
+const products = [
+    {name: "water bottle", price: 50, color: "yellow"}, 
+    {name: "mobile phone", price: 15000, color: "black"},
+    {name: "smart watch", price: 3000, color: 39},
+    {name: "water glass", price: 3, color: "white", color:"pink"}
+]
+
+const productNames = products.map(product => product.name);
+console.log(productNames);
+
+const productPrices = products.map(product => product.price);
+console.log(productPrices);
+
+//to console.log all product of an object
+products.map(product => console.log(product));
+
+//forEach 
+//================================
+//forEach just do the job as map. but dont put it's value in a new array.
+products.forEach(product => console.log(product));
+
+
+
+
+
